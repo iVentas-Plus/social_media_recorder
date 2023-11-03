2191057405c8be9eec6d9ce8cb48c13abad7e8e6
Miguel Acosta <migue.un.av@gmail.com>
2023-11-03 01:43:40 -0600

    Update sound_record_notifier.dart

--- lib/provider/sound_record_notifier.dart
+++ lib/provider/sound_record_notifier.dart
@@ -195,7 +195,7 @@ class SoundRecordNotifier extends ChangeNotifier {
       try {
         RenderBox box = key.currentContext?.findRenderObject() as RenderBox;
         Offset position = box.localToGlobal(Offset.zero);
-        if (position.dx <= MediaQuery.of(context).size.width * 0.7) {
+        if (position.dx <= MediaQuery.of(context).size.width * 0.55) {
           String _time = minute.toString() + ":" + second.toString();
           if (stopRecording != null) stopRecording!(_time);
           resetEdgePadding();
